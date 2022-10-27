693605538b94f37483ab1a3a3e3ad32c82853c88
yewenhua <2574522520@qq.com>
2022-10-27 12:03:39 +0800

    alarm

--- sass/app/Jobs/AlarmDataJob.php
+++ sass/app/Jobs/AlarmDataJob.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 use App\Models\SIS\DcsStandard;
 use App\Models\Mongo\HistorianFormatData;
 use App\Models\MIS\AlarmRule;
