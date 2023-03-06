c8bebdc3f104b248d800b161f7080be7d2ec2c30
yewenhua <2574522520@qq.com>
2023-03-06 16:42:50 +0800

    clas

--- sass/config/class.php
+++ sass/config/class.php
@@ -7,16 +7,16 @@ return [
     //非DCS
     'cass_define' => array(
         array(
+            'name' => '早班',
+            'time' => '23:00-07:30',
+        ),
+        array(
             'name' => '白班',
             'time' => '07:30-16:00',
         ),
         array(
             'name' => '中班',
             'time' => '16:00-23:00',
-        ),
-        array(
-            'name' => '早班',
-            'time' => '23:00-07:30',
         ),
         array(
             'name' => '休息',
