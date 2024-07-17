802e122ef79601588f69d1c7af0d9fde1a0a5018
yewenhua <2574522520@qq.com>
2024-07-17 14:10:25 +0800

    w

--- sass/app/Repositories/WeighBridgeDayDataReposotory.php
+++ sass/app/Repositories/WeighBridgeDayDataReposotory.php
@@ -54,16 +54,32 @@ class WeighBridgeDayDataReposotory extends BaseRepository
         $weighBridgeObj = (new WeighBridgeDayData())->setTable($table);
 
         //获取日期范围内具体数据
-        $datalist = $weighBridgeObj->where('date', '>=', $start)
+        $start = date("Y-m-d", strtotime($start)-24*60*60);
+        $temp = $weighBridgeObj->where('date', '>=', $start)
             ->where('date', '<=', $end)
             ->selectRaw('SUM(value) as val, date')
             ->groupBy('date')
+            ->orderBy('date', 'ASC')
             ->get();
 
-        foreach ($datalist as $key => $item) {
-            $datalist[$key]['val'] = (float)($item->val/1000);
+        $i = 0 ;
+        $datalist = [];
+        $hblist = [];
+        $preValue = 0;
+        foreach ($temp as $key => $item) {
+            $currentValue = (float)($item->val/1000);
+            if($i != 0){
+                $datalist[] = $item;
+                $ratio = $preValue ? 100 * ($currentValue - $preValue)/$preValue : 0;
+                $ratio = strpos($ratio, '.') !== false ? (float)sprintf("%01.2f", $ratio) : $ratio;
+                $hblist[] = $ratio;
+            }
+
+            $preValue = $currentValue;
+            $i++;
         }
         $final['datalist'] = $datalist;
+        $final['hb'] = $hblist;
         $final['en_name'] = config('standard.not_dcs.ljrkl.en_name');
         $final['cn_name'] = config('standard.not_dcs.ljrkl.cn_name');
         $final['messure'] = '吨';
