29177bf417daf08a83bb965e9a0a3a0b5029746e
yewenhua <2574522520@qq.com>
2026-02-27 10:45:43 +0800

    chart

--- sass/app/Http/Controllers/API/HomeController.php
+++ sass/app/Http/Controllers/API/HomeController.php
@@ -125,8 +125,8 @@ class HomeController extends Controller
         $electricityObj = new ElectricityDayDataRepository();
         $weighBridgeObj = new WeighBridgeDayDataReposotory();
 
-        //近365天曲线图
-        $begin_timestamp = time() - 365 * 24 * 60 * 60;
+        //近180天曲线图
+        $begin_timestamp = time() - 180 * 24 * 60 * 60;
         $end_timestamp = time();
         $start = date('Y-m-d', $begin_timestamp);
         $end = date('Y-m-d', $end_timestamp);
